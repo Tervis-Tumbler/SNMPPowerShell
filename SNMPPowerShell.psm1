0d3bcc8a62544d6f80e2b720d08b6023fe0a3703
ChrisMagnuson <hourglasssand@gmail.com>
2017-08-18 15:26:49 -0400

    Initial commit

---
+++ SNMPPowerShell.psm1
@@ -0,0 +1,33 @@
+﻿function Get-SNMPTreeWalk {
+    param (
+        [Parameter(Mandatory)]$ComputerName
+    )
+
+    $SNMP = new-object -ComObject olePrn.OleSNMP
+    $SNMP.open($ComputerName,"public",2,3000)
+    1..1000 | % {
+        $Result = $SNMP.gettree($_)
+        if ($Result) {
+            ConvertFrom-TwoDimensionalArray -Array $Result
+        }
+    }    
+    $SNMP.Close()
+}
+
+
+function ConvertFrom-TwoDimensionalArray {
+    param (
+        [Parameter(Mandatory)]$Array
+    )
+
+    $NumberOfDimensions = $Array.Rank
+    if ($NumberOfDimensions -gt 2 ) { Throw "Array has more than 2 dimensions" }
+    
+    $Object = [PSCustomObject]@{}
+    
+    foreach ($ElementIndex in 0..($Array.GetLength(1)-1)) {
+        $Object | Add-Member -MemberType NoteProperty -Name $Array[0,$ElementIndex] -Value $Array[1,$ElementIndex]
+    }
+
+    $Object
+}
